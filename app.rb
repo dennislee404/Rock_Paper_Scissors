c7c87ed66e16033f96a9f2c53825d861ef7e540b
Dennis Lee <dennislee404@gmail.com>
2023-09-12 12:04:34 +0800

    Can play multiple turns

--- app.rb
+++ app.rb
@@ -9,40 +9,61 @@ end
 post '/start' do 
   @player_name = params[:name]
   @turns = params[:turns].to_i
+  @round  = 1
+  @win = 0
+  @draw = 0
+  @lose = 0
 
   erb :start
 end
 
 post '/result' do 
+
   @player_name = params[:name]
   @turns = params[:turns].to_i
+  @round = params[:round].to_i
+  @win = params[:win].to_i
+  @draw = params[:draw].to_i
+  @lose = params[:lose].to_i
 
   @player_choice = params[:choice]
   @computer_choice = ["rock","paper","scissors"].sample
 
   if @player_choice == @computer_choice
-    @outcome = "It's a TIE!!!"
+    @draw += 1
   else
     if @player_choice == "rock"
       if @computer_choice == "scissors"
-        @outcome = "You WIN!!!"
+        @win += 1
       else
-        @outcome = "You LOSE!!!"
+        @lose += 1
       end
     elsif @player_choice == "paper"
       if @computer_choice == "rock"
-        @outcome = "You WIN!!!"
+        @win += 1
       else
-        @outcome = "You LOSE!!!"
+        @lose += 1
       end
     elsif @player_choice == "scissors"
       if @computer_choice == "paper"
-        @outcome = "You WIN!!!"
+        @win += 1
       else
-        @outcome = "You LOSE!!!"
+        @lose += 1
       end
     end
   end
 
-  erb :result
+  if @win == @lose
+    @outcome = "Its a TIE!!!"
+  elsif @win > @lose
+    @outcome = "You WIN!!!"
+  elsif @lose > @win
+    @outcome = "You LOSE!!!"
+  end
+
+  if @turns > 0
+    erb :start
+  else
+    erb :result
+  end
 end
